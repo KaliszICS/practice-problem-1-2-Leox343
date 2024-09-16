2cfd3582dad40aeb7eb5f6cb10f14b34e397716f
Leox343 <leo.xu6@student.tdsb.on.ca>
2024-09-16 17:55:48 +0000

    hi

--- PP1_2.py
+++ PP1_2.py
@@ -1,29 +1,46 @@
-
+'''
+Lesson 1.2 - Variables, Operators, Concatentation
+made by Leo Xu sept. 16, 2024
+last edited sept. 16, 2024
+'''
 
 
 
 def q1():
   #Write Assignment code here
-
+  name = "Alex"
+  print(name)
 def q2():
   #Write Assignment code here
-
+  num1 = 3
+  num2 = 5
+  num3 = 2
+  print(num1 + num2 + num3)
 def q3():
   #Write Assignment code here
-
+  num21 = 21 
+  num4 = 4
+  print(num21 // num4)
 def q4():
   #Write Assignment code here
-
+  num63 = 6.3
+  num30 = 3.0
+  print(num63 / 3.0)
 def q5():
   #Write Assignment code here
-
+  word = "Hello World"
+  print(word)
+  word = "Goodbye World"
+  print(word)
 def q6():
   #Write Assignment code here
-
+  word = True
+  print(True)
 def q7():
   #Write Assignment code here
-
-  
+  numb1 = 7
+  numb2 = 2
+  print(numb1 % numb2)
 #DO NOT EDIT ANYTHING AFTER THIS COMMENT
 q1()
 q2()
